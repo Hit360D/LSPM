bf65a9e2a196323750447db394114babb95ee18f
Suryansh Patel <49832234+Hit360D@users.noreply.github.com>
2020-03-23 23:56:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,60 @@
 from tkinter import *
+import mysql.connector
 
 #window
 win = Tk()
+win.geometry('1000x1000')
 win.title('LSPM - Least Secure Password Manager')
+
+#---------------------------------------------#
+
+#command module
+def sql_command(type):
+	connection = mysql.connector.connect(host='localhost', database='suryansh', user='suryansh', password='suryansh')
+
+	if connection.is_connected():
+		db_Info = connection.get_server_info()
+		cursor = connection.cursor()
+		cursor.execute("select database();")
+		record = cursor.fetchone()
+		return (db_Info, record)
+
 
 #---------------------------------------------#
 
 def render_frame(selection):
 	if selection == "Show tables":
-		show_tables()
+		show_tables(selection)
 
-	# if selection == 1:
+	# if selection == "Create new table":
 
-	# if selection == 2:
+	# if selection == "Table properties":
 
-	# if selection == 3:
+	# if selection == "Add an entry":
 
-	# if selection == 4:
+	# if selection == "Remove an entry":
 
-	# if selection == 5:
+	# if selection == "Find an entry":
 
-	# if selection == 6:
+	# if selection == "Show all entries":
 
-	# if selection == 7:
+	# if selection == "Remove table":
 
 
 #---------------------------------------------#
 
-def show_tables():
+def show_tables(selection):
 	cframe = Frame(frame, width=500, height=500)
+
+	output = Entry(cframe, borderwidth=0)
+	db_Info, record = sql_command(selection)
+	output.insert(0, "Connected to MySQL Database version " + db_Info)
+	output.pack()
+
 	cframe.pack(expand='true', fill='both', side='top')
-	lab = Label(cframe, text="works")
-	lab.pack()
+
+
+#---------------------------------------------#
 
 
 #---------------------------------------------#
@@ -39,7 +62,7 @@ def show_tables():
 #sidebar
 sidebar = Frame(win, width=200, height=500, bg='#111111')
 
-#to get active selection
+#to get active selection from list
 selection = "nothing"
 def getSel(event):
 	selection = str(listbox.get(listbox.curselection()))
@@ -65,9 +88,6 @@ sidebar.pack(side = 'left', expand = 'false', fill='both')
 #main view
 frame = Frame(win, width=500, height=500, bg='#444444')
 frame.pack(expand='true', fill='both', side='right')
-
-#function to render inside parent frame
-# render_frame(selection, frame)
 
 win.mainloop()
 
