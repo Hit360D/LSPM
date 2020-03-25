e8062f68c63b4fcefa93ea76abfabaeade329eff
Suryansh Patel <49832234+Hit360D@users.noreply.github.com>
2020-03-25 12:53:24 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -11,34 +11,12 @@ style = ttk.Style(win)
 
 #---------------------------------------------#
 
-#command module (under parent = cframe)
-def sql_command(selection):
-	connection = mysql.connector.connect(host='localhost', database='suryansh', user='suryansh', password='suryansh')
-
-	if connection.is_connected():
-		db_Info = connection.get_server_info()
-		cursor = connection.cursor()
-		cursor.execute("select database();")
-		record = cursor.fetchone()
-
-		#sql queries
-		create_table_query = "SHOW tables"
-		cursor = connection.cursor()
-		result = cursor.execute(create_table_query)
-		table = cursor.fetchall()
-		return table
-
-
-
-#---------------------------------------------#
-
 def render_frame(selection):
 	if selection == "Show tables":
 		show_tables(selection)
 
-	# if selection == "Create new table":
-
-	# if selection == "Table properties":
+	if selection == "Create new table":
+		create_table(selection)
 
 	# if selection == "Add an entry":
 
@@ -58,6 +36,22 @@ def show_tables(selection):
 	for child in frame.winfo_children():
 		child.destroy()
 
+	def sql_command(selection):
+		connection = mysql.connector.connect(host='localhost', database='suryansh', user='suryansh', password='suryansh')
+
+		if connection.is_connected():
+			db_Info = connection.get_server_info()
+			cursor = connection.cursor()
+			cursor.execute("select database();")
+			record = cursor.fetchone()
+
+			#sql queries
+			create_table_query = "SHOW tables"
+			cursor = connection.cursor()
+			result = cursor.execute(create_table_query)
+			table = cursor.fetchall()
+			return table
+
 	cframe = Frame(frame, width=500, height=500, bg="#444444")
 
 	#ttk treeview
@@ -75,6 +69,48 @@ def show_tables(selection):
 
 	table_label.pack()
 	tree.pack(side='top', fill='x')
+
+	cframe.pack(expand='true', fill='both', side='top')
+
+
+
+
+
+def create_table(selection):
+	#remove the frame if it already exists
+	for child in frame.winfo_children():
+		child.destroy()
+
+	#get field input and execute code
+	def btn_input(selection, field):
+		field_text = field.get("1.0",'end-1c')
+
+		connection = mysql.connector.connect(host='localhost', database='suryansh', user='suryansh', password='suryansh')
+
+		if connection.is_connected():
+			db_Info = connection.get_server_info()
+			cursor = connection.cursor()
+			cursor.execute("select database();")
+			record = cursor.fetchone()
+
+			create_table_query = "CREATE TABLE " + field_text + " (id int not null AUTO_INCREMENT, website varchar(200) not null, username varchar(200) not null, password varchar(200) not null, primary key (id))"
+			cursor = connection.cursor()
+			result = cursor.execute(create_table_query)
+
+		#to clear text field
+		field.delete(1.0, END)
+
+
+	cframe = Frame(frame, width=500, height=500, bg="#444444")
+
+	text = Label(cframe, text="Table name", width=10, bg="#444444", fg='white')
+	text.grid(row=0, column=0)
+	field = Text(cframe, height=1, width=50)
+	field.grid(row=0, column=1)
+	add_button = Button(cframe, text="Add table", command=lambda: btn_input(selection, field))
+	add_button.grid(row=1, column=1)
+
+
 
 	cframe.pack(expand='true', fill='both', side='top')
 
@@ -98,12 +134,11 @@ def getSel(event):
 listbox = Listbox(sidebar, relief='flat', bd=0, selectmode='single', bg='#111111', fg='#666666')
 listbox.insert(0, "Show tables")
 listbox.insert(1, "Create new table")
-listbox.insert(2, "Table properties")
-listbox.insert(3, "Add an entry")
-listbox.insert(4, "Remove an entry")
-listbox.insert(5, "Find an entry")
-listbox.insert(6, "Show all entries")
-listbox.insert(7, "Remove table")
+listbox.insert(2, "Add an entry")
+listbox.insert(3, "Remove an entry")
+listbox.insert(4, "Find an entry")
+listbox.insert(5, "Show all entries")
+listbox.insert(6, "Remove table")
 
 #get list selection
 listbox.bind('<<ListboxSelect>>', getSel)
